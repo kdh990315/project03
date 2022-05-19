e1529e048db8a4f1172d677f6245da326fa4aab4
kdh990315 <104114383+kdh990315@users.noreply.github.com>
2022-05-19 09:18:22 +0900

    0519

---
+++ main_js/go_top.js
@@ -0,0 +1,15 @@
+// $(function() {
+
+//     $(window).scroll(function() {
+//         if($(this).scrollTop() >= 300) {
+//             $('.go_top').fadeIn();
+//         } else {
+//             $('.go_top').fadeOut();
+//         }
+//     });
+
+//     $('.go_top').click(function(e) {
+//         e.preventDefault();
+//         $('html, body').stop().animate({scrollTop:0}, 500);
+//     });
+// });
