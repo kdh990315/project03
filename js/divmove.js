b76349d7a998fc671eeeee5b3aef9a269dcb65d0
kdh990315 <104114383+kdh990315@users.noreply.github.com>
2022-06-02 01:21:57 +0900

    ffff

--- js/divmove.js
+++ js/divmove.js
@@ -1,13 +1,56 @@
-var scroll_item = document.getElementsByClassName('img1');
-function scrollmove(){
-        if(window.scrollY > scroll_item.offsetTop-700){
-            scroll_item.style.opacity = '1';
-            scroll_item.style.marginTop = '100px';
-            scroll_item.style.transition = 'all 0.4s ease';
-        } else if(window.innerHeight < scroll_item[i].offsetTop){
-            scroll_item.style.opacity = '0';
-            scroll_item.style.marginTop = '300px';
-            scroll_item.style.transition = 'all 0.4s ease';
+$(function() {
+
+    var $main_info_box1 = $('.main_info_box1');
+    var $box2_left_img_box = $('.box2_left_img_box');
+    var $box2_rigth_img_box = $('.box2_rigth_img_box');
+    var $contents = $('.contents');
+    var $test_drive_box = $('.test_drive_box');
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > $main_info_box1.offset().top - 800) {
+            $main_info_box1.find('img').addClass('animate');
+            $main_info_box1.find('.main_info_box1_text').addClass('animate');
         }
-}
-window.addEventListener('scroll',scrollmove);
+
+        if ($(this).scrollTop() > $box2_left_img_box.offset().top - 800) {
+            $box2_left_img_box.addClass('animate1');
+        }
+
+        if ($(this).scrollTop() > $box2_rigth_img_box.offset().top - 800) {
+            $box2_rigth_img_box.addClass('animate2')
+        }
+
+        if ($(this).scrollTop() > $contents.offset().top - 800) {
+            $contents.addClass('animate3')
+        }
+
+        if ($(this).scrollTop() > $('.box1').offset().top - 800) {
+            $('.box1').addClass('animate4')
+        }
+
+        if ($(this).scrollTop() > $('.box2').offset().top - 800) {
+            $('.box2').addClass('animate5')
+        }
+
+        if ($(this).scrollTop() > $('.box3').offset().top - 800) {
+            $('.box3').addClass('animate6')
+        }
+
+        if ($(this).scrollTop() > $('.box4').offset().top - 800) {
+            $('.box4').addClass('animate7')
+        }
+
+        if ($(this).scrollTop() > $('.box5').offset().top - 800) {
+            $('.box5').addClass('animate8')
+        }
+
+        if ($(this).scrollTop() > $('.box6').offset().top - 800) {
+            $('.box6').addClass('animate9')
+        }
+
+        if ($(this).scrollTop() > $test_drive_box.offset().top - 600) {
+            $test_drive_box.addClass('animate10')
+        }
+
+    });
+});
