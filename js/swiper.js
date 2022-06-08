1c7ed9e60f9d64a8263f36962b2b65721a5eacad
kdh990315 <kdh990315@naver.com>
2022-06-08 15:21:22 +0900

    qwdwqd

--- js/swiper.js
+++ js/swiper.js
@@ -6,3 +6,27 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+if (window.matchMedia("(max-width: 768px)").matches) { 
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+} else if (window.matchMedia("(max-width: 480px)").matches) { 
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+}
